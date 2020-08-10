c68362e4762fd73f2f2861a22ac2faca58c49550
Sang Lai <sanglai@lecle.co.kr>
2020-08-10 23:26:43 +0700

    refactor: database config

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 's-oils'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', '12345'),
+        username: env('DATABASE_USERNAME', 'lala'),
+        password: env('DATABASE_PASSWORD', 'lala'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
